6ae8b37b28bc44368eab80be36823c07faa8e58d
Brighton Zhang <75390931+BrightonZhang1@users.noreply.github.com>
2022-05-10 13:12:24 +0000

    FINAL COPY

--- Sketch.java
+++ Sketch.java
@@ -1,187 +1,172 @@
 import processing.core.PApplet;
-
+import processing.core.PImage;  
 /**
- * Description: This program is a game where the player tries to survive by avoiding falling objects
+ * Description: This program lets you decide on what sport you would like to play
  * @author: B. Zhang
  */
 
 public class Sketch extends PApplet {
-  // global variables
-	int intCircleYSpeed = 1;
-  int intPlayerX = 200;
-  int intPlayerY = 350;
-  int intPlayerLives = 3;
-  boolean blnPressDown = false;
-  boolean blnPressUp = false;
-  boolean blnCharW = false;
-  boolean blnCharA = false;
-  boolean blnCharS = false;
-  boolean blnCharD = false;
-  
+  // global variables 
+  PImage imgBasketballCourt;
+  PImage imgSoccerField;
+  PImage imgSoccerBall;
+  PImage imgBasketBall;
+  PImage imgFootball;
+  char charSport = ' ';
+  boolean charP = false;
+  boolean charO = false;
+  boolean charI = false;
+  boolean charU = false;
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
-	// put your size call here
-    size(400, 400);
+    size(900, 500);
   }
-
-  // empty array creation used throughout the program
-  float[] circleY = new float[25];
-  float[] circleX= new float[25];
-  boolean[] ballHideStatus = new boolean[25];
 
   /** 
    * Called once at the beginning of execution.  Add initial set up
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    imgBasketballCourt = loadImage("gym.png");
+    imgBasketballCourt.resize(900,500);
 
-    // creates an array to store random Y values for the snowballs
-    for(int index = 0; index < circleY.length; index++){
-      circleY[index] = random(height - 150);
-    }
+    imgSoccerField = loadImage("soccerfield.jpg");
+    imgSoccerField.resize(900,500);
 
-    // creates an array to store random X values for the snowballs
-    for(int index = 0; index < circleX.length; index++){
-      circleX[index] = random(width);
-    }
+    imgSoccerBall = loadImage("soccerball.png");
+    imgSoccerBall.resize(100,100);
+
+    imgBasketBall = loadImage("basketball.jpg");
+    imgBasketBall.resize(100,100);
+
+    imgFootball = loadImage("football.png");
+    imgFootball.resize(150,100);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-    background(50);
+    background(0);
 
-    // this section of the code sets the speed according to the user inputted arrow keys
-    if (blnPressDown && intCircleYSpeed < 20){
-      intCircleYSpeed ++;
+    // boolean variable mousePressed is used to change the background to a basketball court whenever the user left clicks
+    if (mousePressed){
+      image(imgBasketballCourt, 0, 0);
     }
-    else if (blnPressUp && intCircleYSpeed > 1){
-      intCircleYSpeed --;
-    }
-    
-    // This section of the code creates new snowballs shown on screen using the circleX and circleY arrays
-    for (int index = 0; index < circleY.length; index++) {
+
+    fill(255);
+    textSize(13);
+    text("(Drag your mouse to enter the soccer field)", 50, 25);
+    text("You are pressing this key: ", 50, 50);
+
+    // boolean variable keyPressed is used to track and output the key inputted by the user
+    if (keyPressed){
       fill(255);
-      ellipse(circleX[index], circleY[index], 25, 25);
-      circleY[index] += intCircleYSpeed;
-      
-      if (circleY[index] > height) {
-        circleY[index] = 0;
+      textSize(13);
+      text(key, 200, 50);
+    }
+
+    textSize(30);
+    text("Which is the better sport: ", 250, 100);
+    textSize(18);
+    text("a. Soccer", 250, 125);
+    text("b. Basketball ", 250, 145);
+    text("or Football (hold u,i,o,p)", 250, 165);
+
+    // uses charSport and if statements with a value assigned to it from the keyTyped() function
+    if(charSport == 'a'){
+      textSize(30);
+      text("Heres a soccer ball :)", 250, 205);
+      image(imgSoccerBall, 250, 250);
+      text("Are you sure?? Use the left arrow for no, right arrow for yes", 55, 400);
+      // keyCode is used to track user inputted keys that are not char types and are used to decide on an option.
+      if (keyCode == LEFT) {
+        textSize(18);
+        text("Aww :(", 425, 425);
+      }
+      else if (keyCode == RIGHT) {
+        textSize(18);
+        text("Have another one!", 375, 425);
+        image(imgSoccerBall, 400, 250);
       }
     }
 
-    // This section is responsible for the player movement and tracks user input with the wasd keys
-    fill(0,0,255);
-    ellipse(intPlayerX, intPlayerY, 30, 30);
-    if (blnCharW){
-      intPlayerY -= 3;
-    }
-    else if (blnCharA){
-      intPlayerX -= 3;
-    }
-    else if (blnCharS){
-      intPlayerY += 3;
-    }
-    else if (blnCharD){
-      intPlayerX += 3;
-    }
-
-    // this section is responsible for the edge collision detection with the player
-    if (intPlayerX > width - 15){
-      intPlayerX = width - 15;
-    }
-    else if (intPlayerX < 15){
-      intPlayerX = 15;
-    }
-    else if (intPlayerY < 15){
-      intPlayerY = 15;
-    }
-    else if (intPlayerY > height - 15){
-      intPlayerY = height - 15;
-    }
-
-    // this section helps deduct lives when the player collides with a snowball
-    for(int index = 0; index < circleY.length; index++){
-      if(dist(intPlayerX, intPlayerY, (circleX[index]), circleY[index]) <= 27.5){
-        circleY[index] = 0;
-        intPlayerLives --;
+    // uses charSport for basketball, with a value assigned to it from the keyTyped() function
+    if(charSport == 'b'){
+      textSize(30);
+      text("Heres a basketball :)", 250, 205);
+      image(imgBasketBall, 250, 250);
+      text("Are you sure?? Use the left arrow for no, right arrow for yes", 55, 400);
+      if (keyCode == LEFT) {
+        textSize(18);
+        text("Aww :(", 425, 425);
+      }
+      else if (keyCode == RIGHT) {
+        textSize(18);
+        text("Have another one!", 375, 425);
+        image(imgBasketBall, 400, 250);
       }
     }
 
-    // if the user loses all of their lives, this code sketches a blank screen
-    for(int index = 0; index < intPlayerLives; index++){
-      rect(355 - (index * 30), 20, 20, 20);
-    }
-    if (intPlayerLives <= 0){
-      fill(255);
-      rect(0, 0, width, height);
-    }
-    
-    // ballHideStatus[] tracks whether the user clicks on a snowball, and is responsible for making them disappear
-    for(int index = 0; index < circleY.length; index++){
-      if(dist(mouseX, mouseY, (circleX[index]), circleY[index]) <= 12.5){
-        if(mousePressed){
-					ballHideStatus[index] = true;
-				}
-      }
-    }
-
-    for(int index = 0; index < circleY.length; index++){
-      if(ballHideStatus[index]){
-        circleX[index] = -100;
-      }
+    // multiple booleans to track multiple key inputs for the football path
+    if(charP && charO && charI && charU){
+      image(imgFootball, 675, 275);
+      text("Have a football :)", 675, 225);
     }
   }
 
   /**
-   * Assigns a value used in the draw method that indicates whether a key was pressed
+   * Changes the background to a soccer field, and also creates a cursor for the user when the mouse left click is held down
+   */
+  public void mouseDragged(){
+    image(imgSoccerField, 0, 0);
+    fill(0);
+    ellipse(mouseX, mouseY, 10, 10);
+  }
+
+  /**
+   * Assigns a char type to the charSport variable used in the draw method
+   */
+  public void keyTyped(){
+    charSport = key;
+  }
+
+  /**
+   * Assigns a value for the booleans to use in the draw method that indicate whether a key was pressed
    */
   public void keyPressed() {
-    if (keyCode == DOWN) {
-      blnPressDown = true;
+    if (key == 'p') {
+      charP = true;
     }
-    else if (keyCode == UP) {
-      blnPressUp = true;
+    else if (key == 'o') {
+      charO = true;
     }
-    else if (key == 'w') {
-      blnCharW = true;
+    else if (key == 'i') {
+      charI = true;
     }
-    else if (key == 'a') {
-      blnCharA = true;
-    }
-    else if (key == 's') {
-      blnCharS = true;
-    }
-    else if (key == 'd') {
-      blnCharD = true;
+    else if (key == 'u') {
+      charU = true;
     }
   }
   
   /**
-   * Assigns a value used in the draw method that indicates whether a key was released
+   * Assigns a value for the booleans to use in the draw method that indicate whether a key was released
    */
   public void keyReleased() {
-    if (keyCode == UP) {
-      blnPressUp = false;
+    if (key == 'p') {
+      charP = false;
     }
-    else if (keyCode == DOWN){
-      blnPressDown = false;
+    else if (key == 'o') {
+      charO = false;
     }
-    else if (key == 'w') {
-      blnCharW = false;
+    else if (key == 'i') {
+      charI = false;
     }
-    else if (key == 'a') {
-      blnCharA = false;
-    }
-    else if (key == 's') {
-      blnCharS = false;
-    }
-    else if (key == 'd') {
-      blnCharD = false;
+    else if (key == 'u') {
+      charU = false;
     }
   }
 }
